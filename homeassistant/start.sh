7553d4b7d260d753a5f7086b2cb8d6e6c1dd63f0
Nathan Kuik <nkuik@toogoodtogo.com>
2021-01-10 16:27:22 +0100

    Add pihole, bunch of config

--- homeassistant/start.sh
+++ homeassistant/start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 cp /tmp/configuration.yaml /config/configuration.yaml
-cp -R /tmp/custom_components /config/
 cp -R /tmp/themes /config/
 
 cp /tmp/sensors.yaml /config/sensors.yaml
-cp /tmp/sensors.sh /config/sensors.sh
 cp /tmp/lovelace.sh /config/lovelace.sh
 
-chmod +x /config/sensors.sh
 chmod +x /config/lovelace.sh
 
-if [ ! -f /config/ui-lovelace.yaml ]; then cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml; fi
+cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml
+
+wget -q -O - https://hacs.xyz/install | bash -
 
 python -m homeassistant --config /config
